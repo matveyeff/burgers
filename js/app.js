857ffe2a7f030a5382e977db6ffa7b5735c877d8
Dmitrii Matveev <matveyeff@mail.ru>
2018-11-26 00:27:27 +0700

    added slider

--- js/app.js
+++ js/app.js
@@ -35,3 +35,55 @@ crossicon.addEventListener('click', function() {
 	hamburger.style.display = 'flex';
 	document.getElementById('header').style.position = '';
 })
+
+var team = document.getElementsByClassName('accordeon__item');
+
+for (var i = 0; i < team.length; i++) {
+	team[i].addEventListener('click', function(e) {
+		e.preventDefault();
+		if (!(this.classList.contains('accordeon__item_active'))) {
+			for (var i = 0; i < team.length; i++) {
+				team[i].classList.remove('accordeon__item_active');
+			}
+			this.classList.add('accordeon__item_active');
+		}
+		else if (this.classList.contains('accordeon__item_active')) {
+			for (var i = 0; i < team.length; i++) {
+				team[i].classList.remove('accordeon__item_active');
+			}
+		}
+	})
+}
+
+const left = document.querySelector("#left");
+const right = document.querySelector("#right");
+const slider = document.querySelector("#slider");
+
+const minRight = 0;
+const maxRight = 235;
+const step = 58.75;
+let currentRight = 0;
+
+slider.style.right = currentRight;
+
+right.addEventListener("click", function(e) {
+	e.preventDefault();
+  if (currentRight < maxRight) {
+    currentRight += step;
+    slider.style.right = currentRight + "rem";
+  } else {
+		currentRight = 0;
+		slider.style.right = 0;
+	}
+});
+
+left.addEventListener("click", function(e) {
+	e.preventDefault();
+  if (currentRight > minRight) {
+    currentRight -= step;
+    slider.style.right = currentRight + "rem";
+  } else {
+		currentRight = maxRight;
+		slider.style.right = currentRight + "rem";
+	}
+});
